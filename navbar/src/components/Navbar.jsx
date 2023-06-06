576adf58c7e98702ddfe9fa1a7cd9719f69c09b5
GisellyRock <gisellydsr@gmail.com>
2023-06-06 10:20:39 -0300

    adding responsive

--- navbar/src/components/Navbar.jsx
+++ navbar/src/components/Navbar.jsx
@@ -1,26 +1,27 @@
-import { FaBars, FaTimes } from 'react-icons/fa';
 import { useRef } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import '../Styles/main.css';
 
 function Navbar() {
   const navRef = useRef();
 
   const showNavbar = () => {
-    navRef.current.classlist.toggle('responsive_nav');
+    navRef.current.classList.toggle('responsive_nav');
   };
 
   return (
     <header>
       <h3>Logo</h3>
       <nav ref={navRef}>
-        <a href="/">Home</a>
+        <a href="/#">Home</a>
         <a href="/">My Work</a>
         <a href="/">Blog</a>
         <a href="/">About me</a>
-        <button onClick={showNavbar}>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
           <FaTimes />
         </button>
       </nav>
-      <button onClick={showNavbar}>
+      <button className="nav-btn" onClick={showNavbar}>
         <FaBars />
       </button>
     </header>
